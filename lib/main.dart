bd456bde6b7093db79b1c793e2a17ca4d75b67a7
heemoe <zhonghx_wb@ec.cntaiping.com>
2021-06-22 19:21:27 +0800

    feat: add router strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hacknews_flut/pages/detail.dart';
 import 'components/layout.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  configureApp();
   return runApp(App());
+}
+
+void configureApp() {
+  setUrlStrategy(HashUrlStrategy());
 }
 
 class App extends StatelessWidget {
